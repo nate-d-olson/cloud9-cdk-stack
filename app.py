75d55ae651d82e70726c3bb59fd8fca1e5df0fe1
Nathan D Olson <nolson@nist.gov>
2023-03-22 14:31:51 -0400

    adding permissions boundary

--- app.py
+++ app.py
@@ -19,8 +19,11 @@ ctk = {
     }}
 
 app = cdk.App(context = ctk)
+perm = app.node.try_get_context("@aws-cdk/core:permissionsBoundary")
+print(perm)
+
 cdk_env = cdk.Environment(account="752334989853", region="us-east-1")
 
-Cloud9EnvStack(app, "Cloud9EnvStack", env = cdk_env)
+Cloud9EnvStack(app, "Cloud9EnvStackTest", env = cdk_env)
 
 app.synth()
